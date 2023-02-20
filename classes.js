719b2e2ef659caa8caa6e5c53cc5e8df0180daad
Mill2596 <milla5769@gmail.com>
2023-02-20 14:23:05 +0100

    Billeder og alt muligt

--- classes.js
+++ classes.js
@@ -1,16 +1,23 @@
 class Sun{
-    constructor(x, y, mass, speed, r){
+    constructor(x, y, /*mass,*/ /*speed*/ r){
         this.x = x
         this.y = y
-        this.mass = mass
-        this.speed = speed
+        // this.mass = mass
+        // this.speed = speed
         this.r = r //695508 km
+    }
+
+    show(){
+        noStroke()
+        //fill(255,0,0)
+        image(sun_icon,this.x-50,this.y-50,100,100)
+        //ellipse(this.x,this.y,this.r)
     }
 }
 
-class Planet extends Sun{
-    constructor(){
-        super(x, y, mass, speed, r)
+// class Planet extends Sun{
+//     constructor(){
+//         super(x, y, mass, speed, r)
         
-    }
-}
+//     }
+// }
--- sketch.js
+++ sketch.js
@@ -1,9 +1,22 @@
+let stars;
+let sun;
+let sun_icon;
+
+
+function preload() {
+  stars = loadImage("stars.jpg");
+  sun_icon = loadImage("sun.png");
+}
+
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(windowWidth, windowHeight);
+  sun = new Sun(windowWidth/2, windowHeight/2, 300) 
 }
 
 function draw() {
-  background(220);
+  image(stars,0,0,1368,761)
+  sun.show();
+  console.log(windowWidth, windowHeight)
 }
 
 
