e823804f4b005730fb458b98bc4f60ea7226a752
Mill2596 <milla5769@gmail.com>
2023-02-21 11:04:01 +0100

    jorden bevæger sig

--- classes.js
+++ classes.js
@@ -1,23 +1,37 @@
 class Sun{
-    constructor(x, y, /*mass,*/ /*speed*/ r){
+    constructor(x, y, /*mass,*/ /*speed*/ d){
         this.x = x
         this.y = y
         // this.mass = mass
         // this.speed = speed
-        this.r = r //695508 km
+        this.d = d //695508 km
     }
 
     show(){
         noStroke()
         //fill(255,0,0)
-        image(sun_icon,this.x-50,this.y-50,100,100)
+        image(sun_icon, this.x-this.d/2, this.y-this.d/2, this.d, this.d)
         //ellipse(this.x,this.y,this.r)
     }
 }
 
-// class Planet extends Sun{
-//     constructor(){
-//         super(x, y, mass, speed, r)
-        
-//     }
-// }
+class Planet extends Sun{
+    constructor(x,y,d){
+        super(x, y, /*mass, speed*/d)
+        this.angle = 0
+    }
+
+    move(){
+        //x = x_coord + radius * cos(angle) 
+        this.x = windowWidth/2 + 200 * cos(this.angle);
+        this.y = windowHeight/2 + 200 * sin(this.angle);
+        this.show()
+        this.angle += 1
+        console.log(this.angle)
+    }
+
+    show(){
+        noStroke()
+        image(earth_icon, this.x-this.d/2, this.y-this.d/2,this.d,this.d)
+    }
+}
--- sketch.js
+++ sketch.js
@@ -1,22 +1,27 @@
 let stars;
 let sun;
 let sun_icon;
+let earth_icon;
 
 
 function preload() {
   stars = loadImage("stars.jpg");
   sun_icon = loadImage("sun.png");
+  earth_icon = loadImage("earth.png")
 }
 
 function setup() {
+  angleMode(DEGREES)
   createCanvas(windowWidth, windowHeight);
   sun = new Sun(windowWidth/2, windowHeight/2, 300) 
+  earth = new Planet(windowWidth/3, windowHeight/2,100)
 }
 
 function draw() {
   image(stars,0,0,1368,761)
   sun.show();
-  console.log(windowWidth, windowHeight)
+  earth.move()
+  earth.show()
 }
 
 
